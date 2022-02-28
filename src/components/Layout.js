7ddbf15e142d9f4ccb35f62fdf6a9ad54a7b6fe6
Max Nikitin <teachandtravelcn@gmail.com>
2022-02-28 09:28:06 -0600

    added description

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -46,7 +46,7 @@ class Layout extends Component {
             <Helmet
               title={data.site.siteMetadata.title}
               meta={[
-                { name: 'description', content: 'Spectral' },
+                { name: 'A collection of beautiful code snippets with brilliant solutions for the most popular algos out there, soaked with intellectual efforts and knowledge.', content: '' },
                 { name: 'keywords', content: 'site, web' },
               ]}
             >
